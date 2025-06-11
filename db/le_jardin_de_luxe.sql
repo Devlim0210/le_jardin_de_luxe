d427e110bea088913b2d78bc76ae25383535871a
devlim0210 <ftr.devlim@gmail.com>
2025-06-11 22:54:28 +0200

    ajout de la base de données avevc les produits existants

--- db/le_jardin_de_luxe.sql
+++ db/le_jardin_de_luxe.sql
@@ -1,21 +1,70 @@
--- --------------------------------------------------------
--- Base de données : le_jardin_de_luxe
--- Projet : Boutique en ligne de produits de luxe importés du Japon
--- Auteur : Fetra Henintsoa Ratsimaharison
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : mer. 11 juin 2025 à 22:50
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `le_jardin_de_luxe`
+--
+
 -- --------------------------------------------------------
 
-CREATE DATABASE IF NOT EXISTS le_jardin_de_luxe;
-USE le_jardin_de_luxe;
+--
+-- Structure de la table `produits`
+--
 
--- --------------------------------------------------------
--- Table : produits
--- --------------------------------------------------------
+CREATE TABLE `produits` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `prix` decimal(10,2) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date_ajout` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE IF NOT EXISTS produits (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nom VARCHAR(255) NOT NULL,
-  description TEXT,
-  prix DECIMAL(10,2) NOT NULL,
-  image VARCHAR(255),
-  date_ajout TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT DELAYED IGNORE INTO `produits` (`id`, `nom`, `description`, `prix`, `image`, `date_ajout`) VALUES
+(1, 'sac Dior', 'elegance japonaise', 2000.00, 'tatoo_zeus_barbe.png', '2025-05-23 09:54:07'),
+(2, 'Sac Channel Noir', 'Elegance intemporrelle en cuir', 1500.00, 'sac_channel_noir.jpg', '2025-06-05 09:07:17');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
